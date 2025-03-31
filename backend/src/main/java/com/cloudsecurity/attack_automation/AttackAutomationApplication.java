85c866a3ff9959d3815db8dca42ead12d49672dd
정진택 <87135698+jjt4515@users.noreply.github.com>
2025-03-31 22:22:36 +0900

    first-commit

---
+++ backend/src/main/java/com/cloudsecurity/attack_automation/AttackAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.cloudsecurity.attack_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttackAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttackAutomationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cloudsecurity/attack_automation/AttackAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudsecurity.attack_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttackAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
